14f3a798408bb13106ca3539806ba04310882be9
HoneyDev312 <honeydev312@gmail.com>
2026-02-17 14:18:58 +0100

    manage url error

--- artwork.php
+++ artwork.php
@@ -2,25 +2,33 @@
 require_once(__DIR__ . '/databaseconnect.php');
 require_once(__DIR__ . '/artworks.php');
 
-$artworkID = (int) $_GET["id"];
 
-try {
-    // Ecriture de la requête
-    $sqlQuery = 'SELECT * FROM artworks WHERE artwork_id = :artwork_id';
+$id_error = NULL;
 
-    // Préparation
-    $getArtwork = $mysqlClient->prepare($sqlQuery);
-    // Exécution
-    $getArtwork->execute([
-        'artwork_id' => $artworkID
-    ]);
-    // Fetch
-    $artwork = $getArtwork->fetch(PDO::FETCH_ASSOC);
-} catch (PDOException $e) {
-    // ⚠️ En prod on log plutôt que d'afficher
-    echo "Erreur SQL : " . $e->getMessage();
-    exit;
+if (isset($_GET["id"])) {
+    $artworkID = (int) $_GET["id"];
+    try {
+        // Ecriture de la requête
+        $sqlQuery = 'SELECT * FROM artworks WHERE artwork_id = :artwork_id';
+
+        // Préparation
+        $getArtwork = $mysqlClient->prepare($sqlQuery);
+        // Exécution
+        $getArtwork->execute([
+            'artwork_id' => $artworkID
+        ]);
+        // Fetch
+        $artwork = $getArtwork->fetch(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        // ⚠️ En prod on log plutôt que d'afficher
+        echo "Erreur SQL : " . $e->getMessage();
+        exit;
+    }
+} else {
+    $id_error = !isset($_GET["id"]);
 }
+
+$url_error = $id_error || empty($artwork)
 
 ?>
 
@@ -28,22 +36,26 @@ try {
 <?php require_once(__DIR__ . '/header.php'); ?>
 
 <main>
-
-
-    <article id="detail-artwork">
-        <div id="img-artwork">
-            <img src=<?php echo $artwork["url"] ?> alt=<?php echo $artwork["title"] ?>>
+    <?php if ($url_error) : ?>
+        <div class="error-container">
+            <h3>Désolé, une erreur est survenue, veuillez retourner à la page d'acceuil</h3>
+            <a href="index.php" class="link-home">ACCUEIL</a>
         </div>
-        <div id="content-artwork">
-            <h1><?php echo $artwork["title"] ?></h1>
-            <p class="description"><?php echo $artwork["artist"] ?></p>
-            <p class="description-complete">
-                <?php echo $artwork["description"] ?>
-            </p>
-        </div>
-    </article>
+    <?php else : ?>
+        <article id="detail-artwork">
+            <div id="img-artwork">
+                <img src=<?php echo $artwork["url"] ?> alt=<?php echo $artwork["title"] ?>>
+            </div>
+            <div id="content-artwork">
+                <h1><?php echo $artwork["title"] ?></h1>
+                <p class="description"><?php echo $artwork["artist"] ?></p>
+                <p class="description-complete">
+                    <?php echo $artwork["description"] ?>
+                </p>
+            </div>
+        </article>
 
-
+    <?php endif ?>
 </main>
 
 <!-- inclusion du footer du site -->
