51f44ca7833f54da33fbad8774021fec5b57cf1d
HoneyDev312 <honeydev312@gmail.com>
2026-02-17 05:25:05 +0100

    create array oeuvres.php

--- oeuvre-6.php
+++ oeuvre-6.php
@@ -1,36 +1,20 @@
-<!doctype html>
-<html lang="fr">
+<!-- inclusion du header du site -->
+<?php require_once(__DIR__ . '/header.php'); ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-        content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>The ArtBox</title>
-</head>
+<main>
+    <article id="detail-oeuvre">
+        <div id="img-oeuvre">
+            <img src="img/pawel-czerwinski.png" alt="Chromatics">
+        </div>
+        <div id="contenu-oeuvre">
+            <h1>Chromatics</h1>
+            <p class="description">Jean-Michel Delatronchette</p>
+            <p class="description-complete">
+                Vivamus commodo non libero at hendrerit. In lacinia dui sit amet pellentesque iaculis. Donec at ultricies sem porttitor.
+            </p>
+        </div>
+    </article>
+</main>
 
-<body>
-    <!-- inclusion du header du site -->
-    <?php require_once(__DIR__ . '/header.php'); ?>
-
-    <main>
-        <article id="detail-oeuvre">
-            <div id="img-oeuvre">
-                <img src="img/pawel-czerwinski.png" alt="Chromatics">
-            </div>
-            <div id="contenu-oeuvre">
-                <h1>Chromatics</h1>
-                <p class="description">Jean-Michel Delatronchette</p>
-                <p class="description-complete">
-                    Vivamus commodo non libero at hendrerit. In lacinia dui sit amet pellentesque iaculis. Donec at ultricies sem porttitor.
-                </p>
-            </div>
-        </article>
-    </main>
-
-    <!-- inclusion du footer du site -->
-    <?php require_once(__DIR__ . '/footer.php'); ?>
-</body>
-
-</html>
+<!-- inclusion du footer du site -->
+<?php require_once(__DIR__ . '/footer.php'); ?>
--- oeuvre-8.php
+++ oeuvre-8.php
@@ -1,36 +1,20 @@
-<!doctype html>
-<html lang="fr">
+<!-- inclusion du header du site -->
+<?php require_once(__DIR__ . '/header.php'); ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-        content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>The ArtBox</title>
-</head>
+<main>
+    <article id="detail-oeuvre">
+        <div id="img-oeuvre">
+            <img src="img/steve-johnson-6.png" alt="Blast from the past">
+        </div>
+        <div id="contenu-oeuvre">
+            <h1>Blast from the past</h1>
+            <p class="description">Juliette Baskerville</p>
+            <p class="description-complete">
+                Nunc fermentum purus dapibus justo fermentum auctor. Maecenas non tincidunt leo. Morbi vitae iaculis sem. Donec quis scelerisque massa. Fusce quis accumsan diam, et interdum lectus. Suspendisse mattis pulvinar vehicula. Duis nisi.
+            </p>
+        </div>
+    </article>
+</main>
 
-<body>
-    <!-- inclusion du header du site -->
-    <?php require_once(__DIR__ . '/header.php'); ?>
-
-    <main>
-        <article id="detail-oeuvre">
-            <div id="img-oeuvre">
-                <img src="img/steve-johnson-6.png" alt="Blast from the past">
-            </div>
-            <div id="contenu-oeuvre">
-                <h1>Blast from the past</h1>
-                <p class="description">Juliette Baskerville</p>
-                <p class="description-complete">
-                    Nunc fermentum purus dapibus justo fermentum auctor. Maecenas non tincidunt leo. Morbi vitae iaculis sem. Donec quis scelerisque massa. Fusce quis accumsan diam, et interdum lectus. Suspendisse mattis pulvinar vehicula. Duis nisi.
-                </p>
-            </div>
-        </article>
-    </main>
-
-    <!-- inclusion du footer du site -->
-    <?php require_once(__DIR__ . '/footer.php'); ?>
-</body>
-
-</html>
+<!-- inclusion du footer du site -->
+<?php require_once(__DIR__ . '/footer.php'); ?>
